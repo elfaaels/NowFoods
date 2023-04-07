39d4d339f30f78439318f6fc1127a4dd2168793b
Elfana, A <elfana.anamta@batmandiri.com>
2023-04-07 14:35:35 +0700

    Initial Commit

---
+++ NowFoods/NowFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NowFoodsApp.swift
+//  NowFoods
+//
+//  Created by Elfana Anamta Chatya on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct NowFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
